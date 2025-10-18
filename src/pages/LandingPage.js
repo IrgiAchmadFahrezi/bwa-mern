9a7a8e4df6240d7c1643277645050cc0a6ebda18
IrgiAchmadFahrezi <irgifahrezi78@gmail.com>
2025-10-18 19:27:30 +0700

    first commit

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+import second from 'elements/Button';
+export default class Landingpage extends Component {
+  render() {
+    return <div>Landingpage</div>;
+  }
+}
